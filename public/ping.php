cc447e8a6bebccbc5e2026e012d54224be86789d
Patricio Rodriguez <prodriguez@dintersa.com.ar>
2025-10-23 20:23:53 -0300

    Volver a casa

--- public/ping.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: application/json');
-echo json_encode([
-    "ok" => true,
-    "file" => __FILE__,
-    "cwd"  => getcwd(),
-    "server" => $_SERVER['SERVER_NAME'] ?? null
-]);
