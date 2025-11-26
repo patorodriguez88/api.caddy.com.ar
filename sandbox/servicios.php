d3c95934b7c760125be36a4751f893bcefbe23f6
Patricio Rodriguez <prodriguez@dintersa.com.ar>
2025-11-26 14:11:36 -0300

    htaccess y servicios.php

--- sandbox/servicios.php
+++ sandbox/servicios.php
@@ -3,89 +3,102 @@ require_once 'clases/respuestas.class.php';
 require_once 'clases/servicios.class.php';
 
 $_respuestas = new respuestas;
-$_servicios = new servicios;
+$_servicios  = new servicios;
 
-if ($_SERVER['REQUEST_METHOD'] == "GET") {
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method == "GET") {
+
+    header("Content-Type: application/json");
+
     if (isset($_GET["page"])) {
+
         $pagina = $_GET["page"];
-        $token = $_GET["token"];
-        $estado = $_GET["estado"];
+        $token  = $_GET["token"]  ?? null;
+        $estado = $_GET["estado"] ?? null;
+
         $listaServicios = $_servicios->listaServicios($pagina, $token, $estado);
-        header("Content-Type: application/json");
         echo json_encode($listaServicios);
         http_response_code(200);
     } elseif (isset($_GET['id'])) {
+
         $codigoseguimiento = $_GET['id'];
-        $token = $_GET['token'];
-        $datosPaciente = $_servicios->obtenerSeguimiento($codigoseguimiento, $token);
-        header("Content-Type: application/json");
+        $token             = $_GET['token'] ?? null;
+
+        $datosServicios = $_servicios->obtenerSeguimiento($codigoseguimiento, $token);
         echo json_encode($datosServicios);
         http_response_code(200);
     } elseif (isset($_GET['idProveedor'])) {
+
         $codigoseguimiento = $_GET['idProveedor'];
-        $token = $_GET['token'];
+        $token             = $_GET['token'] ?? null;
+
         $datosServicios = $_servicios->obtenerSeguimientoProveedor($codigoseguimiento, $token);
-        header("Content-Type: application/json");
-        echo json_encode($datosPaciente);
+        echo json_encode($datosServicios);
         http_response_code(200);
+    } else {
+
+        // Sin parámetros válidos
+        $datosArray = $_respuestas->error_400(); // o el que uses
+        echo json_encode($datosArray);
+        http_response_code($datosArray['result']['error_id'] ?? 400);
     }
-} else if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    //recibimos los datos enviados
-    $postBody = file_get_contents("php://input");
-    //enviamos los datos al manejador
+} elseif ($method == "POST") {
+
+    $postBody   = file_get_contents("php://input");
     $datosArray = $_servicios->post($postBody);
-    //delvovemos una respuesta 
+
     header('Content-Type: application/json');
 
     if (isset($datosArray["result"]["error_id"])) {
-
-        $responseCode = $datosArray["result"]["error_id"];
-
-        http_response_code($responseCode);
+        http_response_code($datosArray["result"]["error_id"]);
     } else {
-
         http_response_code(200);
     }
+
     echo json_encode($datosArray);
-} else if ($_SERVER['REQUEST_METHOD'] == "PUT") {
-    //recibimos los datos enviados
-    $postBody = file_get_contents("php://input");
-    //enviamos datos al manejador
+} elseif ($method == "PUT") {
+
+    $postBody   = file_get_contents("php://input");
     $datosArray = $_servicios->put($postBody);
-    //delvovemos una respuesta 
+
     header('Content-Type: application/json');
+
     if (isset($datosArray["result"]["error_id"])) {
-        $responseCode = $datosArray["result"]["error_id"];
-        http_response_code($responseCode);
+        http_response_code($datosArray["result"]["error_id"]);
     } else {
         http_response_code(200);
     }
+
     echo json_encode($datosArray);
-} else if ($_SERVER['REQUEST_METHOD'] == "DELETE") {
-    $headers = getallheaders();
-    if (isset($headers["token"]) && isset($headers["pacienteId"])) {
-        //recibimos los datos enviados por el header
-        $send = [
-            "token" => $headers["token"],
-            "pacienteId" => $headers["pacienteId"]
-        ];
-        $postBody = json_encode($send);
-    } else {
-        //recibimos los datos enviados
-        $postBody = file_get_contents("php://input");
-    }
-    //enviamos datos al manejador
-    $datosArray = $_pacientes->delete($postBody);
-    //delvovemos una respuesta 
-    header('Content-Type: application/json');
-    if (isset($datosArray["result"]["error_id"])) {
-        $responseCode = $datosArray["result"]["error_id"];
-        http_response_code($responseCode);
-    } else {
-        http_response_code(200);
-    }
-    echo json_encode($datosArray);
+    // } elseif ($method == "DELETE") {
+
+    //     $headers = getallheaders();
+
+    //     if (isset($headers["token"]) && isset($headers["pacienteId"])) {
+    //         $send = [
+    //             "token"      => $headers["token"],
+    //             "pacienteId" => $headers["pacienteId"]
+    //         ];
+    //         $postBody = json_encode($send);
+    //     } else {
+    //         $postBody = file_get_contents("php://input");
+    //     }
+
+    //     $datosArray = $_servicios->delete($postBody); // ✅ corregido
+
+    //     header('Content-Type: application/json');
+
+    //     if (isset($datosArray["result"]["error_id"])) {
+    //         http_response_code($datosArray["result"]["error_id"]);
+    //     } else {
+    //         http_response_code(200);
+    //     }
+
+    //     echo json_encode($datosArray);
+
 } else {
+
     header('Content-Type: application/json');
     $datosArray = $_respuestas->error_405();
     echo json_encode($datosArray);
