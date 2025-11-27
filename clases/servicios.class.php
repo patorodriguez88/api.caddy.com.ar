b3ba7436df7942644f82d5941ffdb2a35620cfb0
Patricio Rodriguez <prodriguez@dintersa.com.ar>
2025-11-27 12:56:21 -0300

    Update servicios.class.php

--- clases/servicios.class.php
+++ clases/servicios.class.php
@@ -1,16 +1,5 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-echo "SERVICIOS.CLASS REAL: " . __FILE__ . "<br>";
-
-$path = __DIR__ . '/../conexion/conexion.php';
-echo "VOY A BUSCAR CONEXION EN: " . $path . "<br>";
-
-var_dump(file_exists($path));
-exit;
-
-// require_once "../conexion/conexion.php";
+require_once "../conexion/conexion.php";
 require_once "respuestas.class.php";
 
 class servicios extends conexion
