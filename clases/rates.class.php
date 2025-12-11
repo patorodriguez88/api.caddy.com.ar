4602a7a10d92fea7d22a4fd533b98f7204c3a264
Patricio Rodriguez <prodriguez@dintersa.com.ar>
2025-12-11 17:18:11 -0300

    Update rates.class.php

--- clases/rates.class.php
+++ clases/rates.class.php
@@ -424,12 +424,6 @@ class Rates extends conexion
             'Total'           => $total_label,
 
             'Codigo'          => $codigo,
-            'Debug' => [
-                'SeguroPorc' => $sure_porc,
-                'ValorDec'   => $valorDec,
-                'MinSeguro'  => $this->valorDeclaradoMinimo,
-                'SureRaw'    => $surePrice
-            ],
         ];
 
         return [200, $respuesta];
