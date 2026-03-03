af8d550de4bcf7eefab8db0739643d32d893d26a
Patricio Rodriguez <prodriguez@dintersa.com.ar>
2026-03-03 08:24:23 -0300

    Update warehouse.class.php

--- clases/warehouse.class.php
+++ clases/warehouse.class.php
@@ -1,5 +1,7 @@
 <?php
 // warehouse.class.php
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
 
 require_once __DIR__ . "/conexion/conexion.php";
 require_once __DIR__ . "/respuestas.class.php";
