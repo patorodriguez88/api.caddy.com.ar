01bed65ffd8623f1dd77fcb0af72f6ceed6d5c74
Patricio Rodriguez <prodriguez@dintersa.com.ar>
2026-03-03 08:29:39 -0300

    Update warehouse.class.php

--- clases/warehouse.class.php
+++ clases/warehouse.class.php
@@ -3,9 +3,11 @@
 ini_set('display_errors', '1');
 error_reporting(E_ALL);
 
-require_once dirname(__DIR__) . "/conexion/conexion.php";
-require_once __DIR__ . "/respuestas.class.php";
-require_once __DIR__ . "/token.class.php";  // 👈 ESTE es el correcto
+define('BASE_PATH', __DIR__);
+
+require_once BASE_PATH . "/conexion/conexion.php";
+require_once BASE_PATH . "/clases/respuestas.class.php";
+require_once BASE_PATH . "/clases/token.class.php";  // 👈 ESTE es el correcto
 
 date_default_timezone_set('America/Argentina/Cordoba');
 
