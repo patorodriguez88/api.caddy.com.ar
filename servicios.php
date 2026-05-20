a718ad8d67eb43666b80048108b7c2aa4c7ca904
Patricio Rodriguez <prodriguez@dintersa.com.ar>
2026-05-19 23:27:01 -0300

    Update servicios.php

--- servicios.php
+++ servicios.php
@@ -1,9 +1,4 @@
 <?php
-ob_start();
-
-error_reporting(E_ALL);
-ini_set('display_errors', 0);
-ini_set('log_errors', 1);
 
 require_once __DIR__ . '/conexion/conexion.php';
 require_once __DIR__ . '/clases/respuestas.class.php';
