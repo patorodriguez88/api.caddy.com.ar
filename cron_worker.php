ca42f89e5e51da2a5715bea7af9938ea72c41cff
Patricio Rodriguez <prodriguez@dintersa.com.ar>
2026-08-05 17:32:29 -0300

    Agregar headers no-cache al disparador del worker (nginx lo estaba cacheando)

--- cron_worker.php
+++ cron_worker.php
@@ -14,6 +14,8 @@ if ($secreto !== CRON_WORKER_SECRET) {
 }
 
 header('Content-Type: application/json');
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Pragma: no-cache');
 
 $handler = new auth();
 $worker = new MeliQueueWorker();
